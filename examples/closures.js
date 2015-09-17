208f11c72f474dcde696ef184b97c7d799cf6091
Dana Woodman <dana@danawoodman.com>
2015-09-17 11:06:00 -0700

    Cleanup docs.

--- examples/closures.js
+++ examples/closures.js
@@ -1,14 +1,11 @@
 /**
  * Declaring foo outside of the scope of the sayHi
  * function allows the function to still access the
- * variable within it's "scope"
+ * variable within its "scope"
  *
  * A closure is a special kind of object that combines a
  * function and the environment in which that function
  * was created.
- *
- * Note: Generally speaking, you should be cautious when using
- * closures as they can often lead to complicated code.
  */
 var foo = 'bar'
 
